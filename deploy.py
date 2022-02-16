1c151447f4d8c567658ae01a4ef55116c38ab11b
ilyassber <ilyass.berchida@gmail.com>
2022-02-16 01:28:20 +0100

    deploying contract to Rinkeby test network

--- deploy.py
+++ deploy.py
@@ -1,6 +1,11 @@
-from solcx import compile_standard, install_solc
+import os
 import json
+from dotenv import load_dotenv
+
+from solcx import compile_standard, install_solc
 from web3 import Web3
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -8,6 +13,8 @@ with open("./SimpleStorage.sol", "r") as file:
 # Compile our solidity
 
 install_solc("0.8.11")
+
+print("Compiling Contract...")
 
 compiled_sol = compile_standard(
     {
@@ -37,10 +44,14 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
-w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-chain_id = 1337
-address = "0xb705f6b664C86474c9E19943b1D24c2C6559D1bF"
-private_key = "0x41ee679f8d3218bce42fd788d518195f5da714b0ca5b0219e5ae0d691323424d"
+w3 = Web3(
+    Web3.HTTPProvider("https://rinkeby.infura.io/v3/6c28cb68d69441479a12c48e4c5d0b4f")
+)
+chain_id = 4
+address = "0x7603297a0fB21d8Bdb8de2848c3885138C73E1B2"
+private_key = str(os.getenv("PRIVATE_KEY"))
+
+print("Deploying Contract...")
 
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
@@ -55,4 +66,30 @@ transaction = SimpleStorage.constructor().buildTransaction(
     }
 )
 
-print(transaction)
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key)
+
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+print("Updating Contract...")
+
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": address,
+        "nonce": nonce + 1,
+    }
+)
+
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+
+print(simple_storage.functions.retrieve().call())
